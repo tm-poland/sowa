6959f94cd5b78984988e8ed818227613cea90c71
tm-poland <tmajewski@rzekun.net>
2015-01-26 20:35:18 +0100

    porządki

--- module/sw_co.py
+++ module/sw_co.py
@@ -38,10 +38,7 @@ class Co(threading.Thread):
       
       
       if time.time() - self.t_start >= config.co['t_petli']:
-        #print str(time.time() - self.t_start)
         self.t_start = time.time()
-        
-        #print "dochodzę 10 co"
         
         if status.co['temp_zasilania'] >= \
           config.co['temp_alarm']:  
@@ -53,13 +50,11 @@ class Co(threading.Thread):
           Wylacz('co', '10')
           continue 
           
-        #print "dochodzę 8 co"
         if config.cwu ['wlaczone'] and config.cwu['priorytet'] == True \
           and config.co['gdy_priorytet_cwu'] == False:
           Wylacz('co', '3')
           continue
         
-        #print "dochodzę 7 co"  
         if config.co['wygaszanie'] == True \
           and status.co['rozpalanie'] == False \
           and (status.co['temp_zasilania'] < config.co['temp_min']) \
@@ -67,30 +62,25 @@ class Co(threading.Thread):
           WylaczWygaszanie('4')
           Wylacz('co', '4')
           continue
-          
-        #print "dochodzę 6 co"  
-        
+
         if status.co['wygaszanie'] == False \
           and config.co['wygaszanie'] == False \
           and (status.co['temp_zasilania'] < (config.co['temp_start']- \
           config.co['histereza']/2)):
           Wylacz('co', '5')
           continue
-        #print "dochodzę 5 co"  
         
         if config.co['rozpalanie'] == True \
           and (status.co['temp_zasilania'] >= config.co['temp_rozpalanie']):
           WylaczRozpalanie('6')
           Wlacz('co', '6')
           continue
-        
-        #print "dochodzę 4 co"   
+         
         if status.co['rozpalanie'] == False and (status.co['temp_zasilania'] \
           >= (config.co['temp_start'] + config.co['histereza']/2)):
           Wlacz('co', '7')
           continue
     
-        ##
         if config.co['rozpalanie'] and (status.co['praca'] == False) and (len(self.temp_tab) > 1):
           if (self.temp_tab[len(self.temp_tab) - 1] - self.temp_tab[0]) >= config.co['rozpalanie_delta']:
             Rozpalanie('8')
@@ -98,8 +88,7 @@ class Co(threading.Thread):
           else:
             WylaczRozpalanie('8')
             continue
-            
-        #print "dochodzę 2 co"      
+                  
         if config.co['wygaszanie'] and status.co['praca'] == True \
           and (status.co['temp_zasilania'] <= config.co['temp_wygaszanie']) \
           and (len(self.temp_tab) > 1):
@@ -113,7 +102,6 @@ class Co(threading.Thread):
           Wlacz('co', '10')
           continue 
         
-
       else:
         time.sleep(1)
         
